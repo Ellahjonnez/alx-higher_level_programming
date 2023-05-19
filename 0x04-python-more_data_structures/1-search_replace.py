1e8295f57d315804f76625a4aab2fa9d658f6f2b
Ellahjonnez <ellahjonnez@gmail.com>
2023-05-19 03:48:42 -0700

    Task1:  Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@
 def search_replace(my_list, search, replace):
     """ Creating a new list by applying the
     replacement rule using lambda and map"""
-    
+
     my_new_list = list(map(lambda x: replace if x == search else x, my_list))
 
     return (my_new_list)
