d5fc9b656cddce0f6ea3e654ede66b7c479f8ba8
Ellahjonnez <ellahjonnez@gmail.com>
2023-05-19 04:57:43 -0700

    Task12: Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,9 +2,11 @@
 
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
-        return 0
+        return (0)
 
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000}
+
     result = 0
     prev_value = 0
 
@@ -18,4 +20,4 @@ def roman_to_int(roman_string):
 
         prev_value = value
 
-    return result
+    return (result)
