c372cd346c21ddad56714250573103e399afd121
Ellahjonnez <ellahjonnez@gmail.com>
2023-05-19 03:38:58 -0700

    Tasks0: Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    """ Creating a new matrix by applying the square 
+    """ Creating a new matrix by applying the square
     function to each element using lambda and map"""
 
     result = list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
