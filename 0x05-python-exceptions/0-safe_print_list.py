71ce6c3fc332b5b4d993f5d1f6598f88729bd71b
Ellahjonnez <ellahjonnez@gmail.com>
2023-05-31 09:34:27 -0700

    Task0: Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    # function that prints x elements of a list. #
+
+    my_num = 0
+
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            my_num += 1
+        except IndexError:
+            break
+
+    print("")
+    return (my_num)
