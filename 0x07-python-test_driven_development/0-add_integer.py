b4a1163c8df0471f7b295655b871a09cf20b26ad
Ellahjonnez <ellahjonnez@gmail.com>
2023-06-06 02:39:49 -0700

    Task0: Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,17 +2,18 @@
 
 # a function that adds 2 integers #
 
-def add_integer(a, b=98)
-"""
-> The addition of a and b with args
+def add_integer(a, b=98):
+    
+    """
+    > The addition of a and b with args
 
-> (args): a and b must be integers or floats,
-> Else raise a TypeError exception
-> TypeError: a must be an integer or b must be an integer
-> a and b must be first casted to integers if they are float
-> Returns an integer: the addition of a and b
+    > (args): a and b must be integers or floats,
+    > Else raise a TypeError exception
+    > TypeError: a must be an integer or b must be an integer
+    > a and b must be first casted to integers if they are float
+    > Returns an integer: the addition of a and b
 
-"""
+    """
 
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
