4128252fc7fcea53378db0e069191ba1425bcfe4
Ellahjonnez <ellahjonnez@gmail.com>
2023-06-07 02:48:34 -0700

    Advanced Tasks: Everything is object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 class LockedClass:
+    """ prevents the user from dynamically creating
+    new instance attributes, except if the new
+    instance attribute is called first_name.
+    """
     __slots__ = ['first_name']
-
-    def __setattr__(self, attr, value):
-        if attr != 'first_name':
-            raise AttributeError("Cannot create new instance attributes,"
-                                 "except 'first_name'")
-        self.__dict__[attr] = value
