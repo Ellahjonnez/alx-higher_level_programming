3ea240c218bd3ff8dd212208a957313d1cd9b160
Ellahjonnez <ellahjonnez@gmail.com>
2023-06-07 02:39:08 -0700

    Advanced Tasks: Everything is object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+class LockedClass:
+    __slots__ = ['first_name']
+
+    def __setattr__(self, attr, value):
+        if attr != 'first_name':
+            raise AttributeError
+        ("Cannot create new instance attributes, except 'first_name'")
+        self.__dict__[attr] = value
