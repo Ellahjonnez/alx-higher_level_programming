39c030feb96cd502db8e547f51fca8d2deec010b
Ellahjonnez <ellahjonnez@gmail.com>
2023-06-12 06:21:39 -0700

    Task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,20 +3,14 @@
 """
 A Class MyList that inherits the
 attributes references of class list
-    Args:
-        list: class list
 """
 
 
 class MyList(list):
     """
     The Class that inherits from the list
-    Args:
-        list: class list
     """
 
     def print_sorted(self):
         """ The function that prints the sorted list """
-        sorted_list = self.copy()
-        sorted_list.sort()
-        print(sorted_list)
+        print(sorted(self)
