9aa4df6aea284a5f0f61a42bd2fff88942c87fcb
Ellahjonnez <ellahjonnez@gmail.com>
2023-06-13 16:22:40 -0700

    3. To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Object to JSON string Mode"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+     A function that returns the JSON
+     representation of an object (string):
+    """
+    return json.dumps(my_obj)
