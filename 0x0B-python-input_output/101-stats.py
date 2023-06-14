f9ad548ebac9c327d3a63d80aea4a7f256a6e993
Ellahjonnez <ellahjonnez@gmail.com>
2023-06-13 18:05:03 -0700

    14. Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,39 +5,42 @@
 
 import sys
 
-""" Initialize variables for metrics """
-total_file_size = 0
-status_code_counts = {}
 
-try:
-    line_count = 0
-    for line in sys.stdin:
-        line_count += 1
-        """Split the line based on the specified format"""
-        parts = line.strip().split(' ')
-        ip_address = parts[0]
-        status_code = int(parts[-2])
-        file_size = int(parts[-1])
+def print_stats(status_codes, file_size):
+    """
+    A script that reads stdin line by line and computes metrics:
 
-        """Update total file size"""
-        total_file_size += file_size
+    Arguments:
+        status_codes {dict} >> dictionary of status codes
+        file_size {int} >> file size
+    """
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status_codes.items()):
+        if value > 0:
+            print("{}: {}".format(key, value))
 
-        """Update status code counts"""
-        if status_code in status_code_counts:
-            status_code_counts[status_code] += 1
-        else:
-            status_code_counts[status_code] = 1
 
-        """Print metrics every 10 lines"""
-        if line_count % 10 == 0:
-            print(f"File size: {total_file_size}")
-            for code in sorted(status_code_counts):
-                count = status_code_counts[code]
-                print(f"{code}: {count}")
+if __name__ == "__main__":
+    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+                    '403': 0, '404': 0, '405': 0, '500': 0}
+    file_size = 0
+    count = 0
 
-except KeyboardInterrupt:
-    """Print final metrics on keyboard interruption (CTRL + C)"""
-    print(f"Total file size: {total_file_size}")
-    for code in sorted(status_code_counts):
-        count = status_code_counts[code]
-        print(f"{code}: {count}")
+    try:
+        for line in sys.stdin:
+            count += 1
+            split_line = line.split()
+            try:
+                file_size += int(split_line[-1])
+            except [ValueError, IndexError]:
+                pass
+            try:
+                status_codes[split_line[-2]] += 1
+            except [KeyError, IndexError]:
+                pass
+            if count % 10 == 0:
+                print_stats(status_codes, file_size)
+        print_stats(status_codes, file_size)
+    except KeyboardInterrupt:
+        print_stats(status_codes, file_size)
+        raise
